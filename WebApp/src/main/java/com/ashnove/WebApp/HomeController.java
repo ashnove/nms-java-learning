e9038a4a703b39ba1ed2468e1c7ec88526b264a7
Ashutosh <ashutoshmark18@gmail.com>
2021-09-22 12:54:42 +0530

    model and view class

--- WebApp/src/main/java/com/ashnove/WebApp/HomeController.java
+++ WebApp/src/main/java/com/ashnove/WebApp/HomeController.java
@@ -6,16 +6,18 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 	
-	@RequestMapping("home")
-	public String home(@RequestParam("name") String myName, HttpSession session) {
+	@RequestMapping("home")  //accepting a home request
+	public ModelAndView home(@RequestParam("name") String myName) { // accepting name
 		
-		System.out.println("Hi" + myName);
-		session.setAttribute("name", myName);
-		return "home";
+		ModelAndView mv = new ModelAndView(); // class that holds model and view
+		mv.addObject("name", myName); // data or model
+		mv.setViewName("home"); // view name
+		return mv;
 	}
 }
 //9.0.52
