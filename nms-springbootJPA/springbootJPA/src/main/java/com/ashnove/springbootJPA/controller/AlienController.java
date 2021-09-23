f26f4792333ec564a2beec9ee052bc9674e692b1
Ashutosh <aks.19u10584@btech.nitdgp.ac.in>
2021-09-23 17:19:54 +0530

    postman api calls

--- nms-springbootJPA/springbootJPA/src/main/java/com/ashnove/springbootJPA/controller/AlienController.java
+++ nms-springbootJPA/springbootJPA/src/main/java/com/ashnove/springbootJPA/controller/AlienController.java
@@ -1,17 +1,27 @@
 package com.ashnove.springbootJPA.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ashnove.springbootJPA.dao.AlienRepo;
 import com.ashnove.springbootJPA.model.Alien;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class AlienController {
 	
 	@Autowired
@@ -23,17 +33,41 @@ public class AlienController {
 		return "home.jsp";
 	}
 	
-	@RequestMapping("/addAlien")
-	public String addAlien(Alien alien) {
+	@PostMapping("/alien")
+	public Alien addAlien(@RequestBody Alien alien) {
 		repo.save(alien);
-		return "home.jsp";
+		return alien;
+	}
+	
+	@GetMapping(path = "/aliens", consumes = {"application/json"})
+	public List<Alien> getAlien() {
+		
+		return repo.findAll();
+			
 	}
 	
 	@RequestMapping("/alien/{FID}")
-	@ResponseBody
-	public String getAlien(@PathVariable("FID") int FID) {
+	public Optional<Alien> getAlien(@PathVariable("FID") int FID) {
 		
-		return repo.findById(FID).toString();
+		return repo.findById(FID);
+		 
 	}
+	
+
+	@PutMapping("/File")
+	public Alien updateFile(@RequestBody Alien alien) {
+
+		repo.save(alien);
+		return alien;
+	}
+
+	@DeleteMapping("/alien/{fid}")
+	public String deleteFile(@PathVariable int fid) {
+
+		Alien alien = repo.getOne(fid);
+		repo.delete(alien);
+		return "Object Deleted";
+	}
+	
 	
 }
--- nms-springbootJPA/springbootJPA/src/main/java/com/ashnove/springbootJPA/dao/AlienRepo.java
+++ nms-springbootJPA/springbootJPA/src/main/java/com/ashnove/springbootJPA/dao/AlienRepo.java
@@ -1,5 +1,6 @@
 package com.ashnove.springbootJPA.dao;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -7,11 +8,10 @@ import com.ashnove.springbootJPA.model.Alien;
 
 import java.util.List;
 
-public interface AlienRepo extends CrudRepository<Alien, Integer>{
+public interface AlienRepo extends JpaRepository<Alien, Integer>{
 	
 	List<Alien> findByType(String Type);
-	
-	
+
 	@Query("FROM Alien WHERE type=?1 ORDER BY Name")
 	List<Alien> findByTypeSorted(String type);
 	
